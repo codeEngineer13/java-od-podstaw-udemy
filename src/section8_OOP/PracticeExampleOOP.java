c43066c881477713babc699b321333aa3498bbc6
szysza <techdevpassion@op.pl>
2024-09-09 17:42:25 +0200

    Examples and exercises with static key word

--- src/section8_OOP/PracticeExampleOOP.java
+++ src/section8_OOP/PracticeExampleOOP.java
@@ -1,6 +1,31 @@
 package section8_OOP;
 
+class Plane{
+
+    // wspólny id dla wszystkich obiektów klasy
+    static int nextId = 1;
+
+    // unikalny id dla kazdego obiektu
+    int id;
+
+    Plane(){
+        id=nextId;
+        nextId++;
+    }
+}
+
 public class PracticeExampleOOP {
 
     // There are some example from this section
+    public static void main(String[]args){
+
+        Plane p1 = new Plane();
+        Plane p2 = new Plane();
+        Plane p3 = new Plane();
+
+        System.out.println(p1.id);
+        System.out.println(p2.id);
+        System.out.println(p3.id);
+
+    }
 }
